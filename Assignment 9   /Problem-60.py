06195fd0fad3a514b1f82b6ebc5a8388380e8eb4
SwetaKumariii <sweta.kumari1.cs28@iilm.edu>
2026-04-30 23:41:19 +0530

    Create Problem-60.py

---
+++ Assignment 9   /Problem-60.py
@@ -0,0 +1,22 @@
+def combination_sum(n, k):
+    result = []
+
+    def backtrack(start, path, total):
+        if len(path) == k:
+            if total == n:
+                result.append(path[:])
+            return
+
+        for i in range(start, 10):
+            if total + i > n:
+                break
+            path.append(i)
+            backtrack(i + 1, path, total + i)
+            path.pop()
+
+    backtrack(1, [], 0)
+    return result
+
+
+print(combination_sum(9, 3))
+print(combination_sum(3, 3))
