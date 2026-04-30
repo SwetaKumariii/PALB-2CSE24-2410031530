ee69843be5ba0d0d4fe569d45eada130e402093c
SwetaKumariii <sweta.kumari1.cs28@iilm.edu>
2026-04-30 23:51:53 +0530

    Create Problem-86.py

---
+++ Assignment 10/Problem-86.py
@@ -0,0 +1,6 @@
+def sort_by_length(arr):
+    return sorted(arr, key=len)
+
+
+print(sort_by_length(["GeeksforGeeeks", "I", "from", "am"]))
+print(sort_by_length(["You", "are", "beautiful", "looking"]))
